35f5328f1e040d13735ef2d773533c9ed45b6008
kozyarskaya <92299254+kozyarskaya@users.noreply.github.com>
2021-11-18 11:48:56 +0300

    Update itc_DecToBin.cpp

--- itc_DecToBin.cpp
+++ itc_DecToBin.cpp
@@ -4,14 +4,14 @@ string itc_DecToBin(string str){
     long long i = 0, number = 0, dvnumber = 0;
     string new_str = "", global_str = "";
     while(str[i] != '\0'){
-        if (str[i] == '.'){
+        if (str[i] < '0' || str[i] > '9'){
             number = itc_str_to_int(new_str);
             dvnumber = itc_covert_num(number, 2);
-            global_str += itc_int_to_str(dvnumber) + ".";
+            global_str += itc_int_to_str(dvnumber) + str[i];
 
             new_str = "";
         }
-        if(str[i] != '.')
+        if(str[i] >= '0' && str[i] <= '9')
             new_str += str[i];
 
         i++;
@@ -21,5 +21,3 @@ string itc_DecToBin(string str){
     global_str += itc_int_to_str(dvnumber);
     return global_str;
 }
-
-
